0e6944b5edf34940ca1dd032ec4bfd1ea75ce683
jessicakenza <78642026+jessicakenza@users.noreply.github.com>
2022-05-24 22:19:42 +0200

    Upload message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'CV-Groupe6';
+  urllink: string= "assets/images/dad.jpg";
+
+
+  selectFiles(event:any){
+    console.log(event.target.files);
+    if (event.target.files){
+      var reader = new FileReader()
+      reader.readAsDataURL(event.target.files[0])
+      reader.onload=(event:any)=>{
+        console.log(event);
+        
+        this.urllink=event.target.result
+      }
+    }
+  }
 }
